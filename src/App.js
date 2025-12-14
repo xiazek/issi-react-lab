5bf9529f43a19fcb5c072937e25d981370af23b4
Maciej Książek <maciek@renofi.com>
2025-12-14 10:17:03 +0100

    initial changes on lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = "Wall-E";
+
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My favourite movies to watch</h1>
+      <h2>My favourite movies for today is {title}</h2>
+      <input type="text" onChange={handleChange}/>
     </div>
   );
 }
