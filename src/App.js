3c38c1f1d921c2e5325d369a05fa43f0b50d2a4e
Maciej Książek <maciek@renofi.com>
2026-01-17 10:32:08 +0100

    Added milligram (css feamework) and Adding movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import {useState} from "react";
+import 'milligram'
 
 function App() {
-    const [title, setTitle] = useState('Wall-E');
+    const [title, setTitle] = useState('');
+    const [year, setYear] = useState('');
+    const [movies, setMovies] = useState([]);
 
     let message;
     if (title.length < 5) {
@@ -13,28 +16,49 @@ function App() {
         message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
     }
 
-    function handleChange(event) {
-        setTitle(event.target.value);
+    function handleAddMovie() {
+      const movie = {
+        title: title,
+        year: year,
+      }
+      setMovies([...movies, movie]);
+      console.log(movies);
+      setTitle('');
+      setYear('');
     }
 
-    const movies = [
-        {title: "Wall-E"},
-        {title: "Pulp Fiction"},
-        {title: "Matrix"},
-        {title: "1670"},
-    ];
+
+    // function handleChange(event) {
+    //     setTitle(event.target.value);
+    //     // setYear(event.target.value);
+    // }
+
+    // const movies = [
+    //     {title: "Wall-E"},
+    //     {title: "Pulp Fiction"},
+    //     {title: "Matrix"},
+    //     {title: "1670"},
+    // ];
 
     return (
-        <div>
+        <div className="container">
             <h1>My favourite movies to watch</h1>
             <h2>Titles</h2>
             <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title}</li>)}
+                {movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
             </ul>
-            <h2>My favourite movie for today is {title}</h2>
-            {title.length > 0 && <div>{message}</div>}
-            <input type="text" value={title} onChange={handleChange}/>
-            <button onClick={() => alert(title)}>Pokaż tytuł filmu</button>
+            {/*<h2>My favourite movie for today is {title}</h2>*/}
+            {/*{title.length > 0 && <div>{message}</div>}*/}
+            {/*<input type="text" value={title} onChange={handleChange}/>*/}
+
+            <h2>Add movie</h2>
+
+            <label>Tytuł </label>
+            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
+
+            <label>Rok wydania</label>
+            <input type="text" value={year} onChange={ (event) => setYear(event.target.value)}/>
+            <button onClick={handleAddMovie}>dodaj film</button>
         </div>
     );
 }
